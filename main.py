4a1c7926a48d5270aca78c3ef34034d5d08d93c5
Dimitris <dimitris77_kar@yahoo.gr>
2023-10-31 21:26:23 +0200

    add start button

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from turtle import Turtle, Screen
 import pandas
 import pandas as pd
 
-# set up the screen parameters (background etc)
+# set up the screen parameters (background etc.)
 screen = Screen()
 screen.title("U.S. States Game")
 screen.bgcolor('cadet blue')
@@ -13,32 +13,50 @@ turtle = Turtle()
 turtle.penup()
 turtle.hideturtle()
 
+# create a "start" button
+button = Turtle()
+winn = Screen()
+winn.addshape('button.gif')
+button.shape('button.gif')
 
+
+# read the csv file using pandas and create dataframe
 df = pd.read_csv("50_states.csv")
 
-correct_answers = []
 
-while len(correct_answers) < 50:
-    # ask the user to guess a state
-    answer_state = screen.textinput(title="Guess the State", prompt="What's another State's name?").title()
+def start_game(x, y):
+    # clear the button from screen
+    button.hideturtle()
 
-    if answer_state == "Exit":
-        missing_states = []
+    correct_answers = []
+
+    while len(correct_answers) < 50:
+        # ask the user to guess a state
+        answer_state = screen.textinput(title="Guess the State", prompt="What's another State's name?").title()
+
+        if answer_state == "Exit":
+            missing_states = []
+            for state in df.state:
+                if state not in correct_answers:
+                    missing_states.append(state)
+            new_data = pandas.DataFrame(missing_states)
+            new_data.to_csv("missing_states.csv")
+            break
+        # check if there is such state in U.S.A
         for state in df.state:
-            if state not in correct_answers:
-                missing_states.append(state)
-        new_data = pandas.DataFrame(missing_states)
-        new_data.to_csv("missing_states.csv")
-        break
-    # check if there is such state in U.S.A
-    for state in df.state:
-        if state == answer_state:
-            state_data = df[df.state == answer_state]  # get the corresponding row
-            turtle.goto(int(state_data.x), int(state_data.y))  # write the U.S. state to the correct location
-            turtle.write(answer_state)
-            correct_answers.append(answer_state)
+            if state == answer_state:
+                state_data = df[df.state == answer_state]  # get the corresponding row
+                turtle.goto(int(state_data.x), int(state_data.y))  # write the U.S. state to the correct location
+                turtle.write(answer_state)
+                correct_answers.append(answer_state)
 
-# when the game ends
-turtle.goto(-90, 270)
-turtle.write('GAME OVER', font=('Arial', 19, 'bold'))
+    # when the game ends
+    turtle.goto(-90, 270)
+    turtle.write('GAME OVER', font=('Arial', 19, 'bold'))
 
+
+# when the button clicked, start the game
+screen.listen()
+button.onclick(start_game, btn=1, add=None)
+
+screen.exitonclick()
